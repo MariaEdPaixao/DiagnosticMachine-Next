edfc092db7f090912fd64cf5ee978b18f3cc9dde
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2024-11-03 12:27:25 -0300

    Alterando o use state do ano do carro

--- src/app/cadastro/AnoCarro.tsx
+++ src/app/cadastro/AnoCarro.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 export function AnoCarro({onChange}:AnoProps){
 
-  const [selectedAno, setSelectedAno] = useState<number | undefined>(undefined); // Estado para armazenar o ano selecionado
+  const [selectedAno] = useState<number | undefined>(undefined); // Estado para armazenar o ano selecionado
 
   // Gera a lista de anos entre 1886 e 2025
   const gerarAnos = (inicio: number, fim: number): number[] => {
